dc507bc257490a0e59d15f1debfe00c43063fb44
Alexandros Iosifidis <196984481+LozFunk@users.noreply.github.com>
2025-09-06 12:47:19 +0200

    Added Delete functionalities

--- Backend/6.0 DIY API/index.js
+++ Backend/6.0 DIY API/index.js
@@ -68,11 +68,28 @@ app.patch("jokes/:id", (req, res) =>{
   res.json(replacementJoke);
 });
 
-
-
 //7. DELETE Specific joke
+app.delete("jokes/:id", (req,res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+  res.status(404).json({error: `Joke with the id: ${id} not found. No jokes were deleted`});
+  }
+});
 
 //8. DELETE All jokes
+app.delete("jokes/all", (req, rese)=>{
+  const userKey = req.query.key;
+  if (userKey === masterKey){
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res.status(404).json({error: "You are not authorized to perform this action"});
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
