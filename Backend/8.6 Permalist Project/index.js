f4f2b690daabb6a8806ffdd9b7b6de225f08c24a
Alexandros Iosifidis <196984481+LozFunk@users.noreply.github.com>
2025-09-22 14:56:13 +0200

    Made a simple to do list app

--- Backend/8.6 Permalist Project/index.js
+++ Backend/8.6 Permalist Project/index.js
@@ -1,33 +1,62 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "todo_list",
+  password: "alex1989",
+  port: 5432,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+async function loadItems() {
+  const result = await db.query("SELECT * FROM list_items");
+  let items = result.rows;
+  return items;
+}
 
-app.get("/", (req, res) => {
+// let items = [
+//   { id: 1, title: "Buy milk" },
+//   { id: 2, title: "Finish homework" },
+// ];
+
+app.get("/", async (req, res) => {
+  const items = await loadItems();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  await db.query("INSERT INTO list_items (title) VALUES ($1)",[item]);
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  const updatedId = req.body.updatedItemId;
+  const updatedTitle = req.body.updatedItemTitle;
+  await db.query("UPDATE list_items SET title = ($1) WHERE id = ($2)", [updatedTitle, updatedId])
+  res.redirect("/")
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const deletedId = req.body.deleteItemId
+  await db.query("DELETE FROM list_items WHERE id = ($1)",
+    [deletedId])
+  res.redirect("/")
+
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
